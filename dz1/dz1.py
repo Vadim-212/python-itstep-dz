6616ae82a63ac1313579c1705220117f7da745b8
Vadim <vadim>
2020-02-16 18:05:05 +0600

    Добавление второго домашнего задания

--- dz1/dz1.py
+++ dz1/dz1.py
@@ -1,20 +1,28 @@
-from collections import namedtuple
-from random import sample
-symbols = 'abcdefghijklmnopqrstuvwxyz1234567890'
-employees_info = []
-emp = namedtuple('Employee',('full_name','id','position','login','password'))
+import keyword
+def is_symbols(string):
+    symbols = ['-','.',',','/','\\','|','[',']','{','}','(',')',':',';','!','@','#','$','%','^','&','*','?','~','+','=','\'','"']
+    for i in range(len(string)):
+        if string[i] in symbols:
+            return True
+    return False
 
-with open('info.txt', 'r') as f:
-    lines = f.read().split('\n')
-    for line in lines:
-        employees = line.split(':')
-        full_name = f'{employees[1][0]}. {employees[2][0]}. {employees[3]}'
-        new_login = (employees[1][0] + employees[2][0] + employees[3]).lower()
-        new_password = ''.join(sample(symbols,8))
-        employees_info.append(emp(full_name = full_name, id = employees[0], position = employees[4], login = new_login, password = new_password))
-
-with open('out.html','w') as f:
-    f.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Employees info</title></head><body><table><thead><tr><th>Full name</th><th>Id</th><th>Position</th><th>Login</th><th>Password</th></tr></thead><tbody>')
-    for i, e in enumerate(employees_info):
-        f.write(f'<tr><td>{e.full_name}</td><td>{e.id}</td><td>{e.position}</td><td>{e.login}</td><td>{e.password}</td></tr>')
-    f.write('</tbody></table><style>td, th {padding: 5px 15px;}table, td, th{border: 1px solid black;}</style></body></html>')
+while True:
+    identificator = input('>>> ')
+    if identificator[0].isdigit():
+        print('NOT OK: начинается с цифры')
+    elif keyword.iskeyword(identificator):
+        print('NOT OK: ключевое слово')
+    elif is_symbols(identificator):
+        print('NOT OK: содержит символы')
+    elif identificator[:2] == '__':
+        print('OK: так называют приватные идентификаторы')
+    elif identificator[0] == '_' and identificator[1] != '_':
+        print('OK: так называют защищенные идентификаторы')
+    elif identificator.isupper() and '_' in identificator or identificator.isupper():
+        print('OK: так называют константы')
+    elif identificator[0].isupper() and '_' not in identificator:
+        print('OK: так называют классы')
+    elif identificator.islower() and '_' in identificator:
+        print('OK: так называют переменные')
+    else:
+        print('OK: так называют переменные')
