11dabbc4e24cc97ce97973723b9ac744b00fd40a
Vadim <vadim.212@inbo.ru>
2020-06-24 11:23:52 +0600

    Добавление второй практической

--- practice2/practice2.py
+++ practice2/practice2.py
@@ -1,18 +1,34 @@
 import requests
-import urllib
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
-response = requests.get('https://google.com')
-html_text = response.text
+def get_links(url: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features='html.parser')
+    urls = [v['href'] for v in soup.find_all('a', href=True) if v['href'].startswith('http')]
+    urls = [urlparse(i).netloc for i in urls]
+    unique_urls = []
+    for i in urls: 
+        if i not in unique_urls: 
+            unique_urls.append(i) 
+    counted_urls = []
+    url_counter = 0
+    for url in unique_urls:
+        for u in urls:
+            if url == u:
+                url_counter += 1
+        counted_urls.append((url_counter, url))
+        url_counter = 0
+    counted_urls.sort(key=sortByUrlCount, reverse=True)
+    return counted_urls
 
-#print(html_text[5:10],html_text[:11])
-link = ''
-for i in range(len(html_text) + 6):
-    if html_text[i:i+4] == 'http':
-        #print(html_text[i:i+6])
-        for j in range(len(html_text[i:])):
-            if html_text[j:j+2] != '">':
-                link += html_text[j]
-            else:
-                break
-        #print('\n-------\n',link,'\n-------\n')
-        link = ''
+
+def sortByUrlCount(inputItem: tuple):
+    return inputItem[0]
+
+
+if __name__ == "__main__":
+    url = 'https://google.com/'
+    urls = get_links(url)
+    for url in urls:
+        print(f'{url[0]:<5} {url[1]}')
